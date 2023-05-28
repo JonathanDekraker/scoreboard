df4d61edd15eef6ac1f7ca1a8b0ac875cdb160df
jonathanDekraker <dekraker37@gmail.com>
2023-05-28 15:18:00 -0700

    Testing to see if this automatic resolution scaling would be a good idea or not

--- build/gui.py
+++ build/gui.py
@@ -3,84 +3,92 @@ from tkinter import Tk, Canvas
 def create_canvas():
     window = Tk()
 
-    window.geometry("1366x768")
-    window.configure(bg="#FFFFFF")
+
+    # getting screen's height and width
+    height = window.winfo_screenheight()
+    width = window.winfo_screenwidth()
+    window.geometry(f'{width}x{height}')
+    # Calculate the scale factor based on the screen width and height
+    scale_x = width / 1366
+    scale_y = height / 768
 
     canvas = Canvas(
         window,
         bg="#FFFFFF",
-        height=768,
-        width=1366,
+        height=height,
+        width=width,
         bd=0,
         highlightthickness=0,
         relief="ridge"
     )
 
     canvas.place(x=0, y=0)
-    canvas.create_rectangle(
-        50.0,
-        30.0,
-        954.0,
-        414.0,
-        fill="#D9D9D9",
-        outline="")
 
     canvas.create_rectangle(
-        575.0,
-        70.0,
-        683.0,
-        230.0,
+        50.0 * scale_x,
+        30.0 * scale_y,
+        954.0 * scale_x,
+        414.0 * scale_y,
+        fill="#D9D9D9",
+        outline=""
+    )
+
+    canvas.create_rectangle(
+        575.0 * scale_x,
+        70.0 * scale_y,
+        683.0 * scale_x,
+        230.0 * scale_y,
         fill="#DA0909",
-        outline="")
+        outline=""
+    )
 
     canvas.create_text(
-        595.0,
-        118.0,
+        595.0 * scale_x,
+        118.0 * scale_y,
         anchor="nw",
         text="0",
         fill="#FFFFFF",
-        font=("Inter", 12 * -1),
+        font=("Inter", int(12 * scale_x)),
         tags="away_score"
     )
 
     canvas.create_rectangle(
-        140.0,
-        70.0,
-        248.0,
-        230.0,
+        140.0 * scale_x,
+        70.0 * scale_y,
+        248.0 * scale_x,
+        230.0 * scale_y,
         fill="#DA0909",
-        outline="")
+        outline=""
+    )
 
     canvas.create_text(
-        160.0,
-        118.0,
+        160.0 * scale_x,
+        118.0 * scale_y,
         anchor="nw",
         text="0",
         fill="#FFFFFF",
-        font=("Inter", 12 * -1),
+        font=("Inter", int(12 * scale_x)),
         tags="home_score"
     )
 
     canvas.create_text(
-        382.0,
-        105.0,
+        382.0 * scale_x,
+        105.0 * scale_y,
         anchor="nw",
         text="00:00",
         fill="#000000",
-        font=("Inter", 12 * -1),
+        font=("Inter", int(12 * scale_x)),
         tags="time"
     )
 
     canvas.create_text(
-        323.0,
-        284.0,
+        323.0 * scale_x,
+        284.0 * scale_y,
         anchor="nw",
         text="penalties",
         fill="#000000",
-        font=("Inter", 12 * -1),
-        tags="penalities"
+        font=("Inter", int(12 * scale_x)),
+        tags="penalties"
     )
-
-    window.resizable(False, False)
 
     return canvas
