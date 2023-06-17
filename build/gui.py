40da4051bcdaed35d88773eb2c44fa6a34fccf92
jonathanDekraker <dekraker37@gmail.com>
2023-06-16 22:23:54 -0700

    idk

--- build/gui.py
+++ build/gui.py
@@ -7,276 +7,198 @@ def create_canvas():
     height = 1080
     window.geometry(f'{width}x{height}')
 
-
-    window.configure(bg = "#FFFFFF")
-
-
     canvas = Canvas(
         window,
-        bg = "#FFFFFF",
-        height = 1080,
-        width = 1920,
-        bd = 0,
-        highlightthickness = 0,
-        relief = "ridge"
+        bg="#FFFFFF",
+        height=height,
+        width=width,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge"
     )
 
-    canvas.place(x = 0, y = 0)
+    canvas.place(x=0, y=0)
     canvas.create_rectangle(
-        0.0,
-        0.0,
-        1920.0,
-        1080.0,
+        0, 0, width, height,
         fill="#FFFFFF",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        17.0,
-        20.0,
-        1897.0,
-        1060.0,
+        17, 20, 1897, 1060,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        147.0,
-        197.0,
-        497.0,
-        386.0,
+        147, 197, 497, 386,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        614.0,
-        42.0,
-        1307.0,
-        266.0,
+        614, 42, 1307, 266,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        1111.0,
-        291.0,
-        1217.0,
-        411.0,
+        1111, 291, 1217, 411,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        1424.0,
-        197.0,
-        1774.0,
-        386.0,
+        1424, 197, 1774, 386,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        147.0,
-        197.0,
-        497.0,
-        386.0,
+        670, 69, 886, 237,
+        fill="#000000",
+        outline=""
+    )
+
+    canvas.create_rectangle(
+        1034, 69, 1250, 237,
+        fill="#000000",
+        outline=""
+    )
+
+    canvas.create_rectangle(
+        147, 197, 497, 386,
         fill="#D9D9D9",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        162.0,
-        207.0,
-        482.0,
-        376.0,
+        162, 207, 482, 376,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        1034.494873046875,
-        69.31707763671875,
-        1250.8660125732422,
-        237.5902557373047,
+        614, 42, 1307, 266,
         fill="#D9D9D9",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        1040.4500732421875,
-        74.3779296875,
-        1244.2260284423828,
-        232.4408416748047,
+        624, 52, 1296, 255,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        614.0,
-        42.0,
-        1307.0,
-        266.0,
+        946, 93, 974, 122,
+        fill="#000000",
+        outline=""
+    )
+
+    canvas.create_rectangle(
+        946, 180, 974, 210,
+        fill="#000000",
+        outline=""
+    )
+
+    canvas.create_rectangle(
+        1111, 291, 1217, 411,
         fill="#D9D9D9",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        624.2061767578125,
-        52.92681884765625,
-        1296.7938232421875,
-        255.0731658935547,
+        1117, 297, 1211, 405,
         fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        946.7216796875,
-        93.35610961914062,
-        974.2783813476562,
-        122.85855102539062,
-        fill="#000000",
-        outline="")
-
-    canvas.create_rectangle(
-        946.7216796875,
-        180.77073669433594,
-        974.2783813476562,
-        210.27317810058594,
-        fill="#000000",
-        outline="")
-
-    canvas.create_rectangle(
-        1111.0,
-        291.0,
-        1217.0,
-        411.0,
+        1424, 197, 1774, 386,
         fill="#D9D9D9",
-        outline="")
+        outline=""
+    )
 
     canvas.create_rectangle(
-        1117.0,
-        297.0,
-        1211.8300018310547,
-        405.7200012207031,
+        1439, 207, 1759, 376,
         fill="#000000",
-        outline="")
-
-    canvas.create_rectangle(
-        1424.0,
-        197.0,
-        1774.0,
-        386.0,
-        fill="#D9D9D9",
-        outline="")
-
-    canvas.create_rectangle(
-        1439.0,
-        207.0,
-        1759.0,
-        376.0,
-        fill="#000000",
-        outline="")
-
-    canvas.create_rectangle(
-        670.134033203125,
-        69.31707763671875,
-        886.5051727294922,
-        237.5902557373047,
-        fill="#D9D9D9",
-        outline="")
-
-    canvas.create_rectangle(
-        676.0891723632812,
-        74.3779296875,
-        879.8651275634766,
-        232.4408416748047,
-        fill="#000000",
-        outline="")
-
-    canvas.create_rectangle(
-        1029.0,
-        69.0,
-        1245.3711395263672,
-        237.27317810058594,
-        fill="#D9D9D9",
-        outline="")
-
-    canvas.create_rectangle(
-        1034.955078125,
-        74.06085205078125,
-        1238.7310333251953,
-        232.12376403808594,
-        fill="#000000",
-        outline="")
+        outline=""
+    )
 
     canvas.create_text(
-        907.9381103515625,
-        117.85853576660156,
+        907, 117,
         anchor="nw",
         text="time",
         fill="#FFFFFF",
-        font=("Tinos Regular", 48 * -1)
+        font=("Tinos Regular", 48),
+        tags="time"
     )
 
     canvas.create_text(
-        162.0,
-        207.0,
+        162, 207,
         anchor="nw",
         text="0",
         fill="#FFFFFF",
-        font=("MerriweatherSansRoman Regular", 140 * -1)
+        font=("MerriweatherSansRoman Regular", 140),
+        tags="home_score"
     )
 
     canvas.create_text(
-        1439.0,
-        207.0,
+        1439, 207,
         anchor="nw",
         text="0",
         fill="#FFFFFF",
-        font=("MerriweatherSansRoman Regular", 140 * -1)
+        font=("MerriweatherSansRoman Regular", 140),
+        tags="away_score"
     )
 
     canvas.create_text(
-        23.0,
-        20.0,
+        23, 20,
         anchor="nw",
         text="HOME",
         fill="#DA0909",
-        font=("Karla Bold", 135 * -1)
+        font=("Karla Bold", 135)
     )
 
     canvas.create_text(
-        704.0,
-        296.0,
+        704, 296,
         anchor="nw",
         text="PERIOD",
         fill="#FFFFFF",
-        font=("Karla Bold", 80 * -1)
+        font=("Karla Bold", 80)
     )
 
     canvas.create_text(
-        1117.0,
-        297.0,
+        1117, 297,
         anchor="nw",
         text="1",
         fill="#FFFFFF",
-        font=("Karla Bold", 80 * -1)
+        font=("Karla Bold", 80)
     )
 
     canvas.create_text(
-        1300.0,
-        20.0,
+        1300, 20,
         anchor="nw",
         text="VISITOR",
         fill="#DA0909",
-        font=("Karla Bold", 135 * -1)
+        font=("Karla Bold", 135)
     )
 
     canvas.create_text(
-        681.0,
-        849.0,
+        681, 849,
         anchor="nw",
         text="penalties",
         fill="#FFFFFF",
-        font=("Inter", 36 * -1)
+        font=("Inter", 36),
+        tags="penalties"
     )
 
     canvas.create_rectangle(
-        3.9998779296875,
-        475.5032043457031,
-        1897.0001220703125,
-        488.5032043457031,
+        4, 475.5, 1897, 488.5,
         fill="#FFFFFF",
-        outline="")
+        outline=""
+    )
 
     return canvas
 
